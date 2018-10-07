b720a7b0a3558489e26717a8c4a157b8fef383f7
CarbonylGroup <carbonylgp@gmail.com>
2018-10-07 12:12:19 +0800

    fix header display on mobile

--- src/components/ClubNav.vue
+++ src/components/ClubNav.vue
@@ -1,21 +1,22 @@
 <template>
   <nav class="nav">
-    <h1 v-if="$device.phone" class="nav__title" style="height: 4.5rem;">
-      <router-link to="/" style="text-decoration: none; !important; color: #333333">
-        {{ content.title }}
-        <p style="font-size: 1.5rem; font-weight: 400; margin-top: .5rem; margin-bottom: .5rem">
+    <h1 v-if="$device.phone" class="nav__title" style="height: auto;">
+      <router-link to="/" style="font-size: 2.2rem; text-decoration: none; !important; color: #333333">
+        Club Fair 2018
+        <p style="font-size: 1.2rem; font-weight: 400; margin-top: .5rem; margin-bottom: .5rem">
           MLZJ CLUB ASSOCIATION
         </p>
       </router-link>
     </h1>
-    <h1 v-if="!$device.phone" class="nav__title" style="height: 4.5rem;">
+    <h1 v-if="!$device.phone" class="nav__title" style="height: auto;">
     <img style="position: absolute; width: 4.5rem; height: 4.5rem;" src="../../static/icons/garlands.svg"/>
-    <router-link to="/" style="margin-left: 5.5rem; text-decoration: none; !important; color: #333333">
-      {{ content.title }}
-      <p style="margin-left: 5.5rem; font-size: 1.5rem; font-weight: 400; margin-top: .5rem; margin-bottom: .5rem">
+    <router-link to="/" style="margin-left: 5.5rem; text-decoration: none !important; color: #333333">
+      Club Fair 2018
+    </router-link>
+
+      <p style="margin-left: 5.5rem; font-size: 1.43rem; font-weight: 400 !important; margin-top: .5rem; margin-bottom: .5rem">
         MLZJ CLUB ASSOCIATION
       </p>
-    </router-link>
   </h1>
 
     <transition-group tag="menu" name="nav__item" class="nav__menu">
